ff3430039ac24b2e7359ca981537507cab514825
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-07-19 09:25:23 +0700

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"regexp"
@@ -21,47 +22,56 @@ func main() {
 		input = strings.Replace(input, "\n", "", -1)
 
 		if input != "" {
-			handle(input)
+			result, e := handle(input)
+			if e == nil {
+				fmt.Println(result)
+			} else {
+				fmt.Println(e.Error())
+			}
 		}
 
 	}
 
 }
 
-func handle(input string) {
+func handle(input string) (result string, e error) {
 
 	r := regexp.MustCompile("\\s+")
-	result := r.Split(input, -1)
+	arg := r.Split(input, -1)
 
-	if len(result) == 3 {
-		a, error := strconv.Atoi(result[0])
+	if len(arg) == 3 {
+		a, error := strconv.Atoi(arg[0])
 		if error != nil {
-			fmt.Println(a)
+			e = errors.New("sorry, please type valid operand 1")
 		}
 
-		b, error := strconv.Atoi(result[2])
+		b, error := strconv.Atoi(arg[2])
 		if error != nil {
-			fmt.Println(b)
+			e = errors.New("sorry, please type valid operand 2")
 		}
 
-		switch result[1] {
+		switch arg[1] {
 		case "+":
-			fmt.Println(a + b)
+			result = strconv.FormatInt(int64(a+b), 10)
 		case "-":
-			fmt.Println(a - b)
+			result = strconv.FormatInt(int64(a-b), 10)
+
 		case "*":
-			fmt.Println(a * b)
+			result = strconv.FormatInt(int64(a*b), 10)
+
 		case "/":
-			fmt.Println(a / b)
+			if b == 0 {
+				e = errors.New("sorry, cannot  device to zero")
+
+			} else {
+				result = strconv.FormatFloat(float64(float64(a)/float64(b)), 'g', -1, 64)
+			}
+
 		default:
-			handleError(input)
+			e = errors.New("sorry, please type valid operator")
 		}
 	} else {
-		handleError(input)
+		e = errors.New("sorry, please type valid input")
 	}
-}
-
-func handleError(input string) {
-	fmt.Println("Sorry, please type valid input")
-
+	return
 }
