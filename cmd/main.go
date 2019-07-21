f4526fc99b216a6c2be87ff71ff5c6c48ed6a560
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-07-22 00:04:38 +0700

    update project

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,87 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+)
+
+var globalDB *gorm.DB
+
 func main() {
-	
+	err := connectDB()
+	if err != nil {
+
+		r := gin.Default()
+
+		r.GET("/api/todo", getAllTodo)
+		r.POST("/api/todo", createTodo)
+
+		_ = r.Run(":8080")
+
+	} else {
+		log.Fatal("Cannot connect DB")
+	}
+
+}
+
+type Todo struct {
+	gorm.Model
+	Title     string
+	Completed bool
+}
+
+func connectDB() (err error) {
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=example")
+	defer db.Close()
+
+	if err != nil {
+		return
+	} else {
+		err = db.AutoMigrate(Todo{}).Error
+		if err != nil {
+			log.Fatal("failed to migrate table todos")
+		}
+
+		globalDB = db
+		return
+	}
+}
+
+func createTodo(c *gin.Context) {
+	var argument struct {
+		Title string
+	}
+
+	err := c.BindJSON(&argument)
+	if err != nil {
+		c.String(400, "invalid param")
+		return
+	}
+
+	todo := Todo{
+		Title:     argument.Title,
+		Completed: false,
+	}
+
+	err = globalDB.Create(&todo).Error
+	if err != nil {
+		c.String(500, "failed to create new todo")
+		return
+	}
+
+	c.JSON(200, todo)
+}
+
+func getAllTodo(c *gin.Context) {
+	var todos []Todo
+	err := globalDB.Find(&todos).Error
+
+	if err != nil {
+		c.String(500, "failed to list todolist")
+		return
+	}
+
+	c.JSON(200, todos)
 }
