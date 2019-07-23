dcf97c0c099ffbee9a080211d07dd165255d7c15
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-07-23 19:30:02 +0700

    update with anonymous function

--- cmd/bootcamp/main.go
+++ cmd/bootcamp/main.go
@@ -2,30 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	message := make(chan string)
-	go send(message)
-	go receive(message)
+
+	numbers := make(chan int)
+
+	go func(<-chan int) {
+		for num := range numbers {
+			fmt.Println("Got:", num)
+		}
+	}(numbers)
+
+	go func(chan<- int) {
+		for i := 0; i <= 5; i++ {
+			numbers <- i
+		}
+		close(numbers)
+	}(numbers)
 
 	select {}
-}
-
-func send(message chan<- string) {
-	for {
-		msg := "do"
-		message <- msg
-		fmt.Println("Send:", msg)
-		time.Sleep(1)
-	}
-}
-
-func receive(message <-chan string) {
-	for {
-		msg := <-message
-		fmt.Println("Receive:", msg)
-		time.Sleep(1)
-	}
 }
