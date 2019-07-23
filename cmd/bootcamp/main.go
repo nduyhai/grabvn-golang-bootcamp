7bf85b684b265648340ff88e89a48835dc0f9334
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-07-23 19:17:21 +0700

    add demo concurrency

--- cmd/bootcamp/main.go
+++ cmd/bootcamp/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	
+	message := make(chan string)
+	go send(message)
+	go receive(message)
+
+	select {}
+}
+
+func send(message chan<- string) {
+	for {
+		msg := "do"
+		message <- msg
+		fmt.Println("Send:", msg)
+		time.Sleep(1)
+	}
+}
+
+func receive(message <-chan string) {
+	for {
+		msg := <-message
+		fmt.Println("Receive:", msg)
+		time.Sleep(1)
+	}
 }
