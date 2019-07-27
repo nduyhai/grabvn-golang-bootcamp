31330a5f01c50945b83d0b497ec33ef695cb6fcf
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-07-27 19:17:17 +0700

    add get by id

--- internal/bootcamp/endpoint.go
+++ internal/bootcamp/endpoint.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"log"
+	"strconv"
 )
 
 var globalDB *gorm.DB
@@ -29,6 +30,7 @@ func Handle() {
 		r := gin.Default()
 
 		r.GET("/api/todo", getAllTodo)
+		r.GET("/api/todo/:id", getTodoById)
 		r.POST("/api/todo", createTodo)
 
 		_ = r.Run(":" + config.Server.Port)
@@ -63,20 +65,33 @@ func createTodo(c *gin.Context) {
 	err = globalDB.Create(&todo).Error
 	if err != nil {
 		c.String(500, "failed to create new todo")
-		return
+	} else {
+		c.JSON(200, todo)
 	}
-
-	c.JSON(200, todo)
 }
 
 func getAllTodo(c *gin.Context) {
-	var todos []Todo
-	err := globalDB.Find(&todos).Error
+	var todo []Todo
+	err := globalDB.Find(&todo).Error
 
 	if err != nil {
 		c.String(500, "failed to list todolist")
-		return
+	} else {
+		c.JSON(200, todo)
 	}
 
-	c.JSON(200, todos)
+}
+
+func getTodoById(ctx *gin.Context) {
+	param := ctx.Param("id")
+	id, err := strconv.ParseInt(param, 10, 64)
+	if err == nil {
+		var todo Todo
+		globalDB.Where("id = ?", id).Find(&todo)
+
+		ctx.JSON(200, todo)
+	} else {
+		log.Print(err.Error())
+		ctx.String(500, "failed to get todo")
+	}
 }
